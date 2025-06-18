6de347563423b189c51c73f51058c2f67fe52fd4
saccoViola <168297914+saccoViola@users.noreply.github.com>
2025-06-18 18:54:58 +0200

    final project

---
+++ README.txt
@@ -0,0 +1,12 @@
+Parte 1
+Fare un form con il titolo 
+Il form ti chiede "quante vasche hai fatto oggi?" Inserisci il numero di vasche (fai add) e lo aggiunge alla lista. 
+La frase esatta che deve comparire "[giorno della settimana]: ho fatto X vasche". 
+Possibilità di cancellare l'elemento della lista. 
+
+Parte 2
+Controlla che il valore X sia un numero e non una stringa. 
+Se il numero supera il valore 30 compare un alert con scritto "brava! :)"
+
+Parte 3
+giorno/mese/anno: ho fatto x vasche. 
---
+++ Web Content/script.js
@@ -0,0 +1,66 @@
+const inputBox = document.getElementById("input-box");
+const containerList = document.getElementById("list-container");
+
+const date = new Date(); 
+
+let today = date.toLocaleDateString();
+
+
+
+/*const date = new Date(); 
+let day = today.getDay();
+let daylist = ["domenica", "lunedi'", "martedi'", "mercoledi'", "giovedi'", "venerdi'", "sabato"]; 
+
+console.log("Today is: " + daylist[day] + "."); */
+
+
+
+function addValue(){
+    if(inputBox.value === ""){
+        alert("Non funziona se non inserisci nulla!");
+    }
+    else if(isNaN(inputBox.value) == true){
+        alert("Inserisci un numero!");   
+    }
+    else{
+        let li = document.createElement("LI");
+        containerList.appendChild(li); 
+        li.innerHTML = today + ": ho fatto " + inputBox.value + " vasche.";
+        let span = document.createElement("SPAN");
+        li.appendChild(span); 
+        span.innerHTML = "\u00d7";  
+        if(inputBox.value >= 30){
+            alert("brava! :)")
+        }; 
+    }
+    
+    inputBox.value = "";
+    saveData();
+
+    
+}; 
+
+
+containerList.addEventListener('click', function(e){
+
+ if(e.target.tagName ==="SPAN"){
+        e.target.parentElement.remove();
+        saveData()
+    }
+}, false);
+
+inputBox.addEventListener('keypress', function(e){
+    if(e.key === "Enter"){
+        addValue()
+    }
+});
+
+function saveData(){
+    localStorage.setItem("data", containerList.innerHTML);
+} 
+
+function showRows(){
+    containerList.innerHTML = localStorage.getItem("data");
+} 
+
+showRows(); 
